dfa92aea0b976b4f48bc43fc761eb6d5625257e7
osangmin <osangmin@218.38.137.27>
2018-08-15 23:39:51 +0900

    initila commit

---
+++ src/main/java/com/javalec/ex/MainClass.java
@@ -0,0 +1,16 @@
+package com.javalec.ex;
+
+import org.springframework.context.support.AbstractApplicationContext;
+import org.springframework.context.support.GenericXmlApplicationContext;
+
+public class MainClass {
+//	location	/Users/osangmin/eclipse-workspace/spirng_exam
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+			AbstractApplicationContext ctx = new GenericXmlApplicationContext("classpath:applicationCTX.xml");
+			Pencil pencil = ctx.getBean("pencil", Pencil.class);
+			pencil.use();
+			ctx.close();
+	}
+
+}
---
+++ src/main/java/com/javalec/ex/Pencil6BWithEraser.java
@@ -0,0 +1,11 @@
+package com.javalec.ex;
+
+public class Pencil6BWithEraser implements Pencil {
+
+	@Override
+	public void use() {
+		// TODO Auto-generated method stub
+		System.out.println("6B이며 지우개를 가지고 있습니다.");
+	}
+
+}
